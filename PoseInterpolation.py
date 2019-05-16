8d4e641afa1e6a9179e90c92f7d5bd24ba2bcd1c
VuongTungLong <longvt@VuongTungLongs-MacBook-Pro.local>
2019-05-16 17:49:26 +0100

    fix task2-3

--- PoseInterpolation.py
+++ PoseInterpolation.py
@@ -9,20 +9,19 @@ if __name__ == '__main__':
 	Tracking2D  = read_tracking_data(arg.data_dir, arg.ingore_confidence)
 	Tracking2D = Tracking2D.astype(float)
 	full_list = find_full_matrix(Tracking2D, 50)
-	
-	###### Task 1-3 ########
-	# A = Tracking2D[arg.reference[0]:arg.reference[1]].T
-	# A1 = Tracking2D[arg.target[0]:arg.target[1]].T
-	
-	# A_star_13, IUT, TTU1TA1R = interpolation_13(A, A1)
-
 
 	# method 1
-	resultA1 = []
-	resultA0 = []
+	M1_resultA1 = []
+	M1_resultA0 = []
+	# method 2
+	M2_resultA1 = []
+	M2_resultA0 = []
+
 	for current_frame_shift in arg.shift_arr:
-		tmpA1 = []
-		tmpA0 = []
+		M1_tmpA1 = []
+		M1_tmpA0 = []
+		M2_tmpA1 = []
+		M2_tmpA0 = []
 		for num_missing in arg.missing_joint:
 			# dim1 = frame, dim2 = joint
 			A = np.copy(Tracking2D[arg.reference[0]:arg.reference[0]+arg.length])
@@ -30,34 +29,24 @@ if __name__ == '__main__':
 
 			A1zero = get_random_joint(A1, arg.length, num_missing)
 			A1_star_13, A0_star_13 = interpolation_13(A, A1zero)
+			M1_tmpA1.append(np.around(calculate_mse(A1, A1_star_13), decimals = 3))
+			M1_tmpA0.append(np.around(calculate_mse(A, A0_star_13), decimals = 3))
 
-			print(A1-A1_star_13)
-			tmpA1.append(np.around(calculate_mse(A1, A1_star_13), decimals = 3))
-			tmpA0.append(np.around(calculate_mse(A, A0_star_13), decimals = 3))
-			break
-		resultA1.append(tmpA1)
-		resultA0.append(tmpA0)
-		print(resultA1, resultA0)
-		break
+			A1_star_24, A0_star_24 = interpolation_24(A, A1zero)
+			M2_tmpA1.append(np.around(calculate_mse(A1, A1_star_13), decimals = 3))
+			M2_tmpA0.append(np.around(calculate_mse(A, A0_star_13), decimals = 3))
+
+			
+		M1_resultA1.append(M1_tmpA1)
+		M1_resultA0.append(M1_tmpA0)
+		M2_resultA1.append(M2_tmpA1)
+		M2_resultA0.append(M2_tmpA0)
+	print('M1', M1_resultA1, M1_resultA0)
+	print('M2', M2_resultA1, M2_resultA0)
+
 	target = [arg.reference[0]+0, arg.reference[0]+arg.length+0]
 
-	contruct_skeletion_to_video(arg.input_dir, A1_star_13, target, arg.output_dir, arg.output_video, arg.ingore_confidence)	
-	show_video(arg.output_dir + '/' + arg.output_video, 200)
+	#contruct_skeletion_to_video(arg.input_dir, A1_star_13, target, arg.output_dir, arg.output_video, arg.ingore_confidence)	
+	#show_video(arg.output_dir + '/' + arg.output_video, 200)
 
-	########################
-
-	###### Task 2-4 ########
-	# Ar1 = Tracking2D[arg.reference1[0]:arg.reference1[1]].T
-	# Ar2 = Tracking2D[arg.reference2[0]:arg.reference2[1]].T
-	# A = np.concatenate((Ar1, Ar2), axis=0)
-	# A1 = Tracking2D[arg.target[0]:arg.target[1]].T
-
-	# A_star_24, VTI, A1V1FR = interpolation_24(A, A1)
-	########################
-	
-	###### Task  5  ########
-	# A_star_5 = interpolation(A1, IUT, TTU1TA1R, VTI, A1V1FR)
-	#print(A_star_5.shape)	
-	########################
-	
 	
--- task5.py
+++ task5.py
@@ -31,7 +31,13 @@ def deficiency_matrix(AA, AA1):
 
 
 def interpolation_13(AA, AA1):
-	A, A0, A1, A1_MeanMat, A0_MeanMat = deficiency_matrix(AA, AA1)
+	A = np.copy(AA)
+	A1 = np.copy(AA1)
+	print(A.shape)
+	print(A1.shape)
+	A#0 = np.copy(A)
+	#A0[np.where(A1 == 0)] = 0
+	A, A0, A1, A1_MeanMat, A0_MeanMat = deficiency_matrix(A, A1)
 
 	U = mysvd(np.matmul(A, A.T))
 	U0 = mysvd(np.matmul(A0, A0.T)) 
@@ -47,6 +53,7 @@ def interpolation_13(AA, AA1):
 	A1star = A1star + A1_MeanMat
 	A0star = A0star + A0_MeanMat
 
+	print(A1star)
 	# for task 5
 	joint_length = A1star.shape[0]
 	frame_length = A1star.shape[1]
@@ -54,18 +61,15 @@ def interpolation_13(AA, AA1):
 	I = np.identity(frame_length)
 	IUT = np.kron(I, U.T)
 
-	A1 = A1 + A1_MeanMat
-	A0 = A0 + A0_MeanMat
+	#print(np.where(A1 == 0))
+	#print(Astar[np.where(A1 == 0)])
 
 	A1[np.where(A1 == 0)] = A1star[np.where(A1 == 0)]
 	A0[np.where(A0 == 0)] = A0star[np.where(A0 == 0)]
+	return A1.T, IUT, TTU1TA1.reshape(joint_length*frame_length, 1), A0.T
 
-	return A1.T, A0.T
-
-
-def interpolation_24(AA, AA1):
-	A, A0, A1, A1_MeanMat, A0_MeanMat = deficiency_matrix(AA, AA1)
-
+def interpolation_24(A, A1):
+	#A_new, A0_new, A1_new, A1_MeanMat= deficiency_matrix(A, A1)
 	A0 = A
 	A0[np.where(A1 == 0)] = 0
 
@@ -76,34 +80,44 @@ def interpolation_24(AA, AA1):
 	V1 = mysvd(np.matmul(A1.T, A1))
 
 	A1V1F = np.matmul(np.matmul(A1, V1), F)
-	A0V0F = np.matmul(np.matmul(A0, V0), F)
-	A1star =  np.matmul(A1V1F, V.T)
-	A0star =  np.matmul(A0V0F, V.T)
-
-	A1star = A1star + A1_MeanMat
-	A0star = A0star + A0_MeanMat
+	Astar =  np.matmul(A1V1F, V.T)
 
 	# for task 5
-	joint_length = A1star.shape[0]
-	frame_length = A1star.shape[1]
+	joint_length = Astar.shape[0]
+	frame_length = Astar.shape[1]
 
 	I = np.identity(joint_length)
 	VTI = np.kron(V.T, I)
 
-	A1 = A1 + A1_MeanMat
-	A0 = A0 + A0_MeanMat
+	A1[np.where(A1 == 0)] = Astar[np.where(A1 == 0)]
+	return A1.T, VTI, A1V1F.reshape(joint_length*frame_length, 1)
 
-	A1[np.where(A1 == 0)] = A1star[np.where(A1 == 0)]
-	A0[np.where(A0 == 0)] = A0star[np.where(A0 == 0)]
-	#return A1.T, VTI, A1V1F.reshape(joint_length*frame_length, 1)
-	return A1.T, A0.T
+# AX=B
+def interpolation(A1, IUT, TTU1TA1R, VTI, A1V1FR):
+	A = np.concatenate((IUT, VTI), axis=0)
+	B = np.concatenate((TTU1TA1R, A1V1FR), axis=0)
+	X = np.linalg.lstsq(A, B)
+	A1 = X[0].reshape(A1.shape[0],A1.shape[1])
+	return A1.T
+
+'''
+def random_drop_joint(A, num_drop=[3, 5]):
+	dims = A.shape
+	A = A.reshape(dims[0], 25, int(dims[1] / 25))
+	for i in range(dims[0]):
+		drop = np.random.randint(num_drop[0]) + num_drop[1] - num_drop[0] + 1
+		indices = np.random.randint(25, size=drop)
+		for idx in indices:
+			A[i, idx] = 0
+	return A
+'''
 
 
 def calculate_mse(X, Y):
 	mse = (np.square(X - Y)).mean()
-	#print(X)
-	#print("check")
-	#print(Y)
+	print(X)
+	print("check")
+	print(Y)
 	return mse
 
 
@@ -115,6 +129,6 @@ def get_random_joint(A, length, num_joint_missing):
 	for x in missing_frame_arr:
 		for xx in range(num_joint_missing):
 			indices = random.randint(0, 24)
-			AA[x, indices*2] = 0
-			AA[x, indices*2+1] = 0
+			AA[indices*2, x] = 0
+			AA[indices*2+1, x] = 0
 	return AA
