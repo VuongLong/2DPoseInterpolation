47f36ff5b2e24ee2769b713ac7a6e0d759c8361b
Hai Dang Kieu <dangkh_58@vnu.edu.vn>
2020-02-25 17:08:29 +0000

    add integrating normalization of weighted T method part 2

--- arguments.py
+++ arguments.py
@@ -4,7 +4,7 @@ class arguments(argparse.Namespace):
 
 	length3D = 100
 	reference_task4_3D = [[450, 550]]
-	reference_task4_3D_source = [[50, 150]]
+	reference_task4_3D_source = [[50, 150], [150, 250], [250, 350], [350, 450]]
 
 	ingore_confidence = True
 
